4be66ea9edcd0967828d2a697b455bfc162c9268
Adam Roderick <aroder@gmail.com>
2020-01-16 12:40:00 -0700

    implemented postgres

--- macros/try_cast.sql
+++ macros/try_cast.sql
@@ -9,7 +9,7 @@
 
 {%- macro redshift__try_cast(datatype, str) -%}
 {%- if datatype == 'timestamp' -%}
-  case when left(trim({{str}}), 19) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}( |T)[0-9]{2}:[0-9]{2}:[0-9]{2}' then left(trim({{str}}), 19) else null end::{{datatype}}
+  case when left(trim({{str}}), 19) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}(( |T)[0-9]{2}:[0-9]{2}:[0-9]{2}){0,1}' then left(trim({{str}}), 19) else null end::{{datatype}}
 {%- elif datatype == 'date' -%}
   case 
     when left(trim({{str}}), 5) similar to '[0-9]{4}-' then trim({{str}}) 
@@ -24,3 +24,22 @@
   {{ exceptions.raise_compiler_error("The macro try_cast did not recognize the datatype: " + datatype)}}
 {%- endif -%}
 {%- endmacro -%}
+
+{%- macro postgres__try_cast(datatype, str) -%}
+{%- if datatype == 'timestamp' -%}
+  case when left(trim({{str}}), 19) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}(( |T)[0-9]{2}:[0-9]{2}:[0-9]{2}){0,1}' then left(trim({{str}}), 19) else null end::{{datatype}}
+{%- elif datatype == 'date' -%}
+  case 
+    when left(trim({{str}}), 5) similar to '[0-9]{4}-' then trim({{str}}) 
+    when left(trim({{str}}), 7) similar to '[0-9]{2}-%%%-' then trim({{str}})
+    else null 
+  end::{{datatype}}
+{%- elif datatype == 'int' or datatype == 'bigint' -%}
+  case when trim({{str}}) ~ '^[0-9]+$' then trim({{str}}) else null end::{{datatype}}
+{%- elif datatype.startswith('decimal(') or datatype.startswith('numeric(') -%}
+  case when replace(replace(trim({{str}}), ',', ''), '$', '') similar to '[0-9]+(.[0-9]{2})*' then replace(replace(trim({{str}}), ',', ''), '$', '') else null end::{{datatype}}
+{%- else -%}
+  {{ exceptions.raise_compiler_error("The macro try_cast did not recognize the datatype: " + datatype)}}
+{%- endif -%}
+{%- endmacro -%}
+
