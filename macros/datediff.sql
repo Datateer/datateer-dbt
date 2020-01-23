07fb3a66787b4971a769085f47c22e8b11179147
Adam Roderick <aroder@gmail.com>
2020-01-23 06:49:57 -0700

    whitespace improvements

--- macros/datediff.sql
+++ macros/datediff.sql
@@ -1,20 +1,9 @@
-{% macro datediff(first_date, second_date, datepart) %}
+{%- macro datediff(first_date, second_date, datepart) -%}
   {{ adapter_macro('datateer.datediff', first_date, second_date, datepart) }}
-{% endmacro %}
-
-
-{% macro default__datediff(datepart, first_date, second_date) %}
-
-    datediff(
-        '{{ datepart }}',
-        {{ first_date }},
-        {{ second_date }}
-        )
-
-{% endmacro %}
-
-
-{% macro postgres__datediff(datepart, first_date, second_date) %}
+{%- endmacro -%}
+{%- macro default__datediff(datepart, first_date, second_date) -%}
+    datediff('{{ datepart }}', {{ first_date }}, {{ second_date }})
+{%- endmacro -%}
+{%- macro postgres__datediff(datepart, first_date, second_date) -%}
     DATE_PART('{{datepart}}', cast({{second_date}} as timestamp) - cast({{first_date}} as timestamp))
-
-{% endmacro %}
+{%- endmacro -%}
