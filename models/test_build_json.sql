1fbd846a5f2a9bbe939d2e661ed8e516a18cc347
Adam Roderick <aroder@gmail.com>
2020-03-13 12:06:03 -0600

    removed extraneous model file

--- models/test_build_json.sql
+++
@@ -1,8 +0,0 @@
-{% set fields=['my_custom_field', 'better_name:field2'] %}
-
-with cte as (
-    select 'my custom value' as my_custom_field,
-        'value 2' as field2
-)
-select {{ build_json(fields) }} as example
-from cte
