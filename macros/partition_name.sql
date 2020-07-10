7a9d53b60a946f99bfcf8b4922eaf2b7c7ab6bde
Adam Roderick <aroder@gmail.com>
2020-07-10 01:22:17 -0600

    patition_name

--- macros/partition_name.sql
+++ macros/partition_name.sql
@@ -2,6 +2,7 @@
   Specifically designed to overcome a shortcoming in dbt-external-tables. See https://github.com/fishtown-analytics/dbt-external-tables/issues/33
   This macro will just return the value that is passed into it
 -#}
-{%- macro passthrough(name, value) -%}
-  {{ return(value) }}
+{%- macro partition_name(name, value) -%}
+  {% set path = 'export_date=' + value %}
+  {{ return(path) }}
 {%- endmacro -%}
