260d5a4680ef696d1281df18c45b4c86d5788b2e
Sergey <sergeylabetsik@datateer.com>
2022-08-18 14:46:31 -0500

    Fix try_cast macro

--- macros/try_cast.sql
+++ macros/try_cast.sql
@@ -9,9 +9,21 @@
 
 {%- macro bigquery__try_cast(datatype, str) -%}
 {%- if datatype == 'timestamp' or datatype == 'datetime' -%}
-  safe_cast('{{str}}' as datetime)
-{%- elif datatype == 'date' or datatype == 'int' or datatype == 'bigint' or datatype == 'decimal' -%}
-  safe_cast('{{str}}' as {{datatype}})
+  safe_cast(case
+      when regexp_contains(left(trim(str), 19), '[0-9]{4}-[0-9]{2}-[0-9]{2}(( |t)[0-9]{2}:[0-9]{2}:[0-9]{2}){0,1}') then left(trim(str), 19)
+      when regexp_contains(left(trim(str), 11), '[0-9]{2}-[a-za-z]{3}-[0-9]{4}') then left(trim(str), 11)
+      when regexp_contains(left(trim(str), 10), '[0-9]{2}-[0-9]{2}-[0-9]{4}') then left(trim(str), 10)
+      else null
+    end as {{datatype}})
+{%- elif datatype == 'date' -%}
+  safe_cast(case
+      when regexp_contains(left(trim(str), 19), '[0-9]{4}-[0-9]{2}-[0-9]{2}') then left(trim(str), 10)
+      when regexp_contains(left(trim(str), 11), '[0-9]{2}-[a-za-z]{3}-[0-9]{4}') then left(trim(str), 11)
+      when regexp_contains(left(trim(str), 10), '[0-9]{2}-[0-9]{2}-[0-9]{4}') then left(trim(str), 10)
+      else null
+    end as {{datatype}})
+{%- elif datatype == 'int' or datatype == 'bigint' or datatype == 'decimal' -%}
+  safe_cast({{str}} as {{datatype}})
 {%- else -%}
   {{ exceptions.raise_compiler_error("The macro try_cast did not recognize the datatype: " + datatype)}}
 {%- endif -%}
@@ -20,18 +32,18 @@
 
 {%- macro redshift__try_cast(datatype, str) -%}
 {%- if datatype == 'timestamp' -%}
-  case 
-    when left(trim({{str}}), 19) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}(( |T)[0-9]{2}:[0-9]{2}:[0-9]{2}){0,1}' then left(trim({{str}}), 19) 
+  case
+    when left(trim({{str}}), 19) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}(( |T)[0-9]{2}:[0-9]{2}:[0-9]{2}){0,1}' then left(trim({{str}}), 19)
     when left(trim({{str}}), 11) similar to '[0-9]{2}-[A-Za-z]{3}-[0-9]{4}' then left(trim({{str}}), 11)
-	when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
-    else null 
+  when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
+    else null
   end::{{datatype}}
 {%- elif datatype == 'date' -%}
-  case 
-    when left(trim({{str}}), 10) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}' then trim({{str}}) 
+  case
+    when left(trim({{str}}), 10) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}' then left(trim({{str}}), 10)
     when left(trim({{str}}), 11) similar to '[0-9]{2}-[A-Za-z]{3}-[0-9]{4}' then left(trim({{str}}), 11)
-	when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
-    else null 
+  when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
+    else null
   end::{{datatype}}
 {%- elif datatype == 'int' or datatype == 'bigint' -%}
   case when trim({{str}}) ~ '^[0-9]+$' then trim({{str}}) else null end::{{datatype}}
@@ -44,18 +56,18 @@
 
 {%- macro postgres__try_cast(datatype, str) -%}
 {%- if datatype == 'timestamp' -%}
-  case 
-    when left(trim({{str}}), 19) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}(( |T)[0-9]{2}:[0-9]{2}:[0-9]{2}){0,1}' then left(trim({{str}}), 19) 
+  case
+    when left(trim({{str}}), 19) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}(( |T)[0-9]{2}:[0-9]{2}:[0-9]{2}){0,1}' then left(trim({{str}}), 19)
     when left(trim({{str}}), 11) similar to '[0-9]{2}-[A-Za-z]{3}-[0-9]{4}' then left(trim({{str}}), 11)
-	when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
-    else null 
+  when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
+    else null
   end::{{datatype}}
 {%- elif datatype == 'date' -%}
-  case 
-    when left(trim({{str}}), 10) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}' then trim({{str}}) 
+  case
+    when left(trim({{str}}), 10) similar to '[0-9]{4}-[0-9]{2}-[0-9]{2}' then left(trim({{str}}), 10)
     when left(trim({{str}}), 11) similar to '[0-9]{2}-[A-Za-z]{3}-[0-9]{4}' then left(trim({{str}}), 11)
-	when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
-    else null 
+  when left(trim({{str}}), 10) similar to '[0-9]{2}-[0-9]{2}-[0-9]{4}' then left(trim({{str}}), 10)
+    else null
   end::{{datatype}}
 {%- elif datatype == 'int' or datatype == 'bigint' -%}
   case when trim({{str}}) ~ '^[0-9]+$' then trim({{str}}) else null end::{{datatype}}
@@ -65,4 +77,3 @@
   {{ exceptions.raise_compiler_error("The macro try_cast did not recognize the datatype: " + datatype)}}
 {%- endif -%}
 {%- endmacro -%}
-
