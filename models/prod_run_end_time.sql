5ea4f9924eb8db3e57e14f181ce729f8dfb2b9e5
Chris Marchetti <cmarche2ti@gmail.com>
2022-11-10 10:09:01 -0700

    Added yml file to run model in prod only

--- models/prod_run_end_time.sql
+++ models/prod_run_end_time.sql
@@ -2,8 +2,6 @@
 -- timestamp of the last successful dbt run. Use case is a 
 -- DbtTask in the prefect orchestration that runs after 
 -- dbt test.
-{% if flags.prod %}
+
 select current_timestamp as last_successful_prod_run
-{% else %}
-select null as last_successful_prod_run
-{% endif %}
+
